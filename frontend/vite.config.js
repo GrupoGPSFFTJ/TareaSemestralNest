b791ce376e1970e3475f38d37c1152f149cd6758
yvnsdev <tomas.yevenes2101@alumnos.ubiobio.cl>
2025-12-14 17:57:10 -0300

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'https://eventix-manager.onrender.com'
+    ]
+  },
   server: {
     proxy: {
       '/api': {
