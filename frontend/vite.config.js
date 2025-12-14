c3214e874ee73f8bef88742d49426e19fbe4856c
yvnsdev <tomas.yevenes2101@alumnos.ubiobio.cl>
2025-12-14 17:59:14 -0300

    fixing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     allowedHosts: [
-      'https://eventix-manager.onrender.com'
+      'eventix-manager.onrender.com'
     ]
   },
   server: {
