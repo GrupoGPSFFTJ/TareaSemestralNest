e6fe80cf807831f82a4b0af9d5ac6a07dbc78e17
favodev <fernando.aurelio.ortiz@gmail.com>
2025-12-13 22:35:00 -0300

    arerglo de  cors y dependencias faltantes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true,
+  });
   
   app.useGlobalPipes(
     new ValidationPipe({
